d1708506ce439365278c759be0bb67a2993038c3
Tobias Gauster <t.gauster@boku.ac.at>
2015-07-29 13:21:58 +0200

    using quantile function for plot, adding L-moments

--- notebook/distributions.R
+++ notebook/distributions.R
@@ -2,12 +2,11 @@ library(lmom)
 
 
 #' # TODO
+#' 4) Reversieren einer Verteilung veranschaulichen
 #'
-#' 1) for schleifen in den plot funktionen
-#' 2) plot der cdf lt Konvention und Hydrologie
-#' 4) plot der cdf f. $L$-Momente
 
 #' # Weibullverteilung
+#'
 #' linksseitig begrenzt
 #'
 #' ## 1) stats::Weibull
@@ -19,7 +18,7 @@ library(lmom)
 
 #' cdf: $F(x) = 1 - e^{- \left(\frac{x}{scale}\right)^{shape}} = 1 - e^{- (x/b)^a}$ für $x > 0$
 #'
-#' density: $f(x) = \frac{a}{b} \left(\frac{x}{b}\right)^{(a-1)} e^{- (x/b)^a}$
+#' pdf: $f(x) = \frac{a}{b} \left(\frac{x}{b}\right)^{(a-1)} e^{- (x/b)^a}$
 #'
 #' `lower.tail = TRUE` per default: also für Minima parametrisiert
 #'
@@ -35,25 +34,88 @@ library(lmom)
 #' * k > 0 is the shape parameter
 #' * λ > 0 is the scale parameter
 #'
-#' cdf: $F(x;k,\lambda) = 1 - e^{-\left(\frac{x}{scale}\right)^{shape}} = 1 - e^{-(x/\lambda)^k}$
+#' pdf: $f(x;\lambda,k) = \frac{k}{\lambda}\left(\frac{x}{\lambda}\right)^{k-1}e^{-(x/\lambda)^{k}}$
 #'
-#' density: $f(x;\lambda,k) = \frac{k}{\lambda}\left(\frac{x}{\lambda}\right)^{k-1}e^{-(x/\lambda)^{k}}$
 
-x <- seq(0, 2.5, length.out=1000)
-plot(x, dweibull(x, .5), type="l", col="blue", xlim=c(0, 2.5), ylim=c(0, 2.5),
+#' cdf: $F(x;k,\lambda) = 1 - e^{-\left(\frac{x}{scale}\right)^{shape}} = 1 - e^{-(x/\lambda)^k}$
+
+#+ fig.width = 10, echo  = FALSE
+col <- c("blue", "red", "black", "green")
+prob <- seq(0, 1, 0.001)
+
+
+par(mfcol = c(1, 2))
+plot(NA, xlim=c(0, 2.5), ylim=c(0, 1), type = "n", xlab = "x", ylab = "P(X <= x)",
      main = "Variation des shape Parameters")
-lines(x, dweibull(x, 1), type="l", col="red")
-lines(x, dweibull(x, 1.5), type="l", col="magenta")
-lines(x, dweibull(x, 5), type="l", col="green")
-legend("topright", legend=paste("scale (\u03bb) = 1, shape (k) =", c(.5, 1, 1.5, 5)), lwd=1, col=c("blue", "red", "magenta", "green"))
+
+shape <- c(0.5, 1, 1.5, 5)
+for(i in seq_along(shape)) {
+  lines(qweibull(prob, shape = shape[i], scale = 1), prob, type="l", col=col[i])
+}
+
+legend("bottomright", lwd=1, col=col, cex = 0.75,
+       legend=paste("scale (\u03bb) = 1, shape (k) =", shape))
 
 
-plot(x, dweibull(x, shape = 1.5, .5), type="l", col="blue", xlim=c(0, 2.5), ylim=c(0, 2.5),
+plot(NA, xlim=c(0, 5), ylim=c(0, 1), type = "n", xlab = "x", ylab = "P(X <= x)",
      main = "Variation des scale Parameters")
-lines(x, dweibull(x, shape = 1.5, 1), type="l", col="magenta")
-lines(x, dweibull(x, shape = 1.5, 1.5), type="l", col="red")
-lines(x, dweibull(x, shape = 1.5, 5), type="l", col="green")
-legend("topright", legend=paste("shape (k) = 1.5, scale (\u03bb) = ", c(.5, 1, 1.5, 5)), lwd=1, col=c("blue", "magenta", "red", "green"))
+
+scale <- c(0.5, 1.5, 1, 5)
+for(i in seq_along(scale)) {
+  lines(qweibull(prob, shape = 1.5, scale = scale[i]), prob, type="l", col=col[i])
+}
+
+legend("bottomright", lwd=1, col=col, cex = 0.75,
+       legend=paste("scale (\u03bb) = ", scale, " shape (k) = 1.5"))
+
+
+
+
+
+par(mfcol = c(1, 2))
+plot(NA, xlim=c(0, 2.5), ylim=c(0, 1), type = "n", xlab = "x", ylab = "P(X <= x)",
+     main = "Herkömmliche Darstellung")
+
+for(i in seq_along(shape)) {
+  lines(qweibull(prob, shape = shape[i], scale = 1), prob, type="l", col=col[i])
+}
+
+legend("bottomright", lwd=1, col=col, cex = 0.75,
+       legend=paste("scale (\u03bb) = 1, shape (k) =", shape))
+
+plot(NA, xlim=c(0, 2.5), ylim=c(0.01, 1), type = "n", xlab = "x", ylab = "P(X <= x)",
+     log = "y", main = "Herkömmliche Darstellung, log")
+
+for(i in seq_along(shape)) {
+  lines(qweibull(prob, shape = shape[i], scale = 1), prob, type="l", col=col[i])
+}
+
+legend("bottomright", lwd=1, col=col, cex = 0.75,
+       legend=paste("scale (\u03bb) = 1, shape (k) =", shape))
+
+
+
+
+plot(NA, ylim=c(0, 2.5), xlim=c(0, 1), type = "n", ylab = "x", xlab = "P(X <= x)",
+     main = "In der Hydrologie übliche Darstellung")
+
+for(i in seq_along(shape)) {
+  lines(prob, qweibull(prob, shape = shape[i], scale = 1), type="l", col=col[i])
+}
+
+legend("topleft", lwd=1, col=col, cex = 0.75,
+       legend=paste("scale (\u03bb) = 1, shape (k) =", shape))
+
+
+plot(NA, ylim=c(0, 2.5), xlim=c(0.01, 1), type = "n", ylab = "x", xlab = "P(X <= x)",
+     log = "x", main = "In der Hydrologie übliche Darstellung, log")
+
+for(i in seq_along(shape)) {
+  lines(prob, qweibull(prob, shape = shape[i], scale = 1), type="l", col=col[i])
+}
+
+legend("topleft", lwd=1, col=col, cex = 0.75,
+       legend=paste("scale (\u03bb) = 1, shape (k) =", shape))
 
 
 
@@ -65,8 +127,31 @@ legend("topright", legend=paste("shape (k) = 1.5, scale (\u03bb) = ", c(.5, 1, 1
 #'
 #' cdf: $F(x) = 1 - e^{ - \left( \frac{x - location} {scale} \right)^{shape} } = 1 - e^{ - \left( \frac{x - \zeta} {\beta} \right)^\delta }$
 
-prob <- seq(0, 1, 0.01)
-plot(x = quawei(f = prob, para = c(0, 1, 5)), type = "l", y = prob)
+
+#+ echo = FALSE, fig.width = 10
+par(mfcol = c(1, 2))
+plot(NA, xlim=c(0, 2.5), ylim=c(0, 1), type = "n", xlab = "x", ylab = "P(X <= x)",
+     main = "Herkömmliche Darstellung")
+
+for(i in seq_along(shape)) {
+  lines(y=prob, x=quawei(f = prob, para = c(0, 1, shape[i])), type="l", col=col[i])
+}
+
+legend("bottomright", lwd=1, col=col, cex = 0.75,
+       legend=paste("scale (\u03bb) = 1, shape (k) =", shape))
 
 
+
+plot(NA, ylim=c(0, 2.5), xlim=c(0, 1), type = "n", ylab = "x", xlab = "P(X <= x)",
+     main = "In der Hydrologie übliche Darstellung")
+
+for(i in seq_along(shape)) {
+  lines(prob, quawei(f = prob, para = c(0, 1, shape[i])), type="l", col=col[i])
+}
+
+legend("topleft", lwd=1, col=col, cex = 0.75,
+       legend=paste("scale (\u03bb) = 1, shape (k) =", shape))
+
+
+# complementary cumulative distribution function (ccdf) or simply the tail distribution or exceedance
 
