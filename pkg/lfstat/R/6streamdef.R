0a6bb1346e0a35890c5bf2e99272bae818e3a6bc
Tobias Gauster <t.gauster@boku.ac.at>
2015-09-24 11:08:32 +0200

    replacing streamdef and with wrapper

--- pkg/lfstat/R/6streamdef.R
+++ pkg/lfstat/R/6streamdef.R
@@ -1,131 +1,59 @@
-#DOTO: Startdate und Enddate übergeben! UNIT!!!
-
+# wrapper around the new function find_droughts() including pooling
+# just emulates the interface of the old and buggy function streamdef()
 streamdef <- function(lfobj,
                       pooling = c("none", "MA", "IT", "IC"),
                       threslevel = 70,
-                      thresbreaks = c("fixed", "monthly", "daily", "seasonal"),
-                      breakdays = c("01/06", "01/10"),
+                      thresbreaks = c("fixed", "monthly", "daily","seasonal"),
+                      breakdays = c("01/06","01/10"),
                       MAdays = 7,
                       tmin = 5,
                       IClevel = 0.1,
-                      mindur = 0, #min days for IT/IC
-                      minvol = 0, #min volume for IT/IC
+                      mindur = 0,
+                      minvol = 0,
                       table = c("all", "volmax", "durmax"),
-                      na.rm = TRUE){
-  lfcheck(lfobj)
+                      na.rm = TRUE
+) {
+
   pooling <- match.arg(pooling)
   thresbreaks <- match.arg(thresbreaks)
   table <- match.arg(table)
 
-  threshold <- buildthres(lfobj = lfobj, threslevel = threslevel,
-                          thresbreaks=thresbreaks, breakdays = breakdays,
-                          na.rm = na.rm)
+  breakdays <- paste(breakdays, "2000", sep = "/")
+  breakdays <- as.Date(breakdays, format = "%d/%m/%Y")
 
-  #Calculation FLOW
-  if(pooling == "MA"){
-    ma2 <- function(x, n){
-      a <- ma(x=x, n = n, sides=2)
-
-      # to avoid NA's -> first and last values are not averaged
-      a[is.na(a)]<-x[is.na(a)]
-
-      return(a)
-    }
+  if(thresbreaks == "fixed") thresbreaks <- "constant"
+  if(thresbreaks == "seasonal") {
+    thresbreaks <- breakdays
   }
 
-  flow <- switch(pooling,
-                 "none" = lfobj$flow,
-                 "IT" = lfobj$flow,
-                 "IC" = lfobj$flow,
-                 "MA" = as.vector(ma2(x = lfobj$flow, n = MAdays)))
+  x <- as.xts(lfobj)
 
-  #Comparing FLOW and THRESHOLD
-  temp <- merge(x=lfobj, y=threshold, by = c("day", "month"), sort = FALSE)
-  temp <- temp[order(temp$year, temp$month, temp$day), ]
+  threshold <- vary_threshold(x = x, varying = thresbreaks,
+                              fun = quantile, probs = (1 - threslevel/100),
+                              na.rm = na.rm)
 
-  defrun <-  rle(flow <= temp$flow.y)
-  pos <- c(cumsum(c(1, defrun$lengths)))
-  streamdef <- data.frame(matrix(ncol = 9))
-  names(streamdef) <-  c("pos", "d", "v", "mi", "Qmin", "startyear", "startmonth", "startday", "hyear")
-  #a <-  pos[defrun$values] # length(defrun$lengths) != length(pos)
-  a <-  pos[which(defrun$values)]
-  a <- a[!is.na(a)]
-  # a <- a[-length(a)] # switched off (GL)
-  d <- defrun$lengths[defrun$values]
-  d <- d[!is.na(d)]
-  def <- flow - temp$flow.y
-  for(ii in seq_along(a)){
-    streamdef[ii, 1] <- a[ii]
-    streamdef[ii, 6] <- lfobj[a[ii], "year"]
-    streamdef[ii, 7] <- lfobj[a[ii], "month"]
-    streamdef[ii, 8] <- lfobj[a[ii], "day"]
-    streamdef[ii, 9] <- lfobj[a[ii], "hyear"]
-    streamdef[ii, 2] <- d[ii]
-    # bug: a[ii] can be out of range in the following line
-    streamdef[ii, 3] <- sum(def[a[ii]:(a[ii]+d[ii]-1)])
-    #  streamdef[ii, 4] <- streamdef[ii, 3]/d[ii]
-    #  streamdef[ii, 5] <- min(flow[a[ii]:(a[ii]+d[ii]-1)])
+  x <- find_droughts(x, threshold = threshold)
+
+  if(pooling == "MA") x <- pool_ma(x, n = MAdays)
+  if(pooling == "IT") x <- pool_ic(x, tmin = tmin, ratio = Inf)
+  if(pooling == "IC") x <- pool_ic(x, tmin = tmin, ratio = IClevel)
+
+  y <- summary(x, drop_minor = c(volume = minvol, duration = mindur))
+  qmin <- tapply(x$discharge, x$event.no, min, na.rm = T)
+
+  yy <- with(y, data.frame(d = duration, v = volume, mi = volume/duration,
+                           Qmin = qmin[-1],
+                           strsplit_date(start, prefix = "start")))
+
+  if(table != "all") {
+    yy$hyear <- water_year(y$start, hyear_start(lfobj))
+    yy <- by(yy, yy$hyear, function(x) x[which.max(x[, substr(table, 1, 1)]), ])
+    yy <- do.call(rbind, yy)
+    rownames(yy) <- NULL
   }
 
-  if(pooling == "IT"){
-    # buggy! inter-event time is neglected
-    for(ii in nrow(streamdef):2){
-      if(streamdef[ii, "pos"]-streamdef[ii-1, "d"]-tmin < streamdef[ii-1, "pos"]){
-        streamdef[ii-1, "d"] <- streamdef[ii, "d"]+streamdef[ii-1, "d"]
-        streamdef[ii-1, "v"] <- streamdef[ii, "v"]+streamdef[ii-1, "v"]
-        streamdef <- streamdef[-ii, ]
-      }
-    }
-  }
-
-  if(pooling == "IC"){
-    # buggy! max 2 events are merged
-    for(ii in nrow(streamdef):2){
-      if(streamdef[ii, "pos"]-streamdef[ii-1, "d"]-tmin < streamdef[ii-1, "pos"]){
-        s <- sum((def[(streamdef[ii-1, "pos"]+streamdef[ii-1, "d"]):(streamdef[ii, "pos"]-1)]))
-        if(-s/streamdef[ii-1, "v"]<IClevel){
-          streamdef[ii-1, "d"] <- streamdef[ii, "pos"]-streamdef[ii-1, "pos"]+streamdef[ii, "d"]
-          streamdef <- streamdef[-ii, ]
-        }
-      }
-    }
-  }
-
-  for(ii in seq_along(streamdef$pos)){
-    if(pooling == "IC"){
-      streamdef[ii, 3] <- sum(def[streamdef[ii, "pos"]:(streamdef[ii, "pos"]+streamdef[ii, "d"]-1)])}
-    streamdef[ii, 4] <- streamdef[ii, "v"]/streamdef[ii, "d"]
-    streamdef[ii, 5] <- min(flow[streamdef[ii, "pos"]:(streamdef[ii, "pos"]+streamdef[ii, "d"]-1)])
-  }
-
-  #excluding minor deficits in IC/IT methods
-  if(pooling %in% c("IC", "IT")){
-    streamdef <- streamdef[streamdef$d >mindur & streamdef$v < -minvol, ]
-  }
-
-  streamdef$v <- -streamdef$v*60**2*24
-  streamdef$mi <- -streamdef$mi*60**2*24
-  if(table == "all") {
-    return(streamdef[, -c(1, 9)])
-  }else{
-    if(table == "volmax"){
-      agg <- aggregate(v ~ hyear, streamdef, max)
-      lin <- NULL
-      for(ii in seq_along(agg$hyear)){
-        lin[ii] <- min(which(streamdef$hyear == agg$hyear[ii] & streamdef$v == agg$v[ii]))
-      }
-      streamdef[lin, -c(1)]
-    }else{
-      agg <- aggregate(d ~ hyear, streamdef, max)
-      lin <- NULL
-      for(ii in seq_along(agg$hyear)){
-        lin[ii] <- min(which(streamdef$hyear == agg$hyear[ii] & streamdef$d == agg$d[ii]))
-      }
-      streamdef[lin, -c(1)]
-    }
-  }
+  return(yy)
 }
-#gives data.frame: pos in lfobj, length of lf, flow deficit
 
 #########################
 #Streamflow plot        #
