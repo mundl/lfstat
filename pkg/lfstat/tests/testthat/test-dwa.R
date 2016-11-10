4d6c8a31272c4d45cbd23601008df2980700e21f
Tobias Gauster <t.gauster@boku.ac.at>
2016-11-10 16:44:47 +0100

    unit tests for fitting of durations (DWA)

--- pkg/lfstat/tests/testthat/test-dwa.R
+++ pkg/lfstat/tests/testthat/test-dwa.R
@@ -22,3 +22,43 @@ test_that("estimated parameters of the distribution are correct", {
                             "50" = 55.0, "100" = 52.2))
 })
 
+
+
+context("evfit reproduces fitting of durations as in DWA Merkblatt")
+
+infile <- read.csv2("borstendorf.csv", header = TRUE,
+                    colClasses = c("Date", "numeric"))
+
+bdorf <- xts(x = infile$discharge, order.by = infile$time)
+flowunit(bdorf) <- "m^3/s"
+hyear_start(bdorf) <- 4
+
+# find droughts und MA pooling
+dr <- find_droughts(bdorf, threshold = 1.79) # MNQ aus der DWA S. 105
+expect_warning(pooled <- pool_ma(dr, n = 7))
+
+# partial series of durations
+partial <- summary(pooled, drop_minor = FALSE)[,c("start", "volume", "duration")]
+partial$hyear <- water_year(partial$start, assign = "start", origin = 4)
+
+annual <- tapply(partial$duration, partial$hyear, max)
+annual[is.na(annual)] <- 0
+annual <- data.frame(time = as.numeric(rownames(annual)), duration = annual)
+
+
+# fit a GEV distribution
+expect_warning(fit <- evfit(annual$duration, "gev", extreme = "maximum"))
+
+
+test_that("parameters are equal to DWA", {
+  evp <- round(as.numeric(fit$parameters$gev), 4)
+  expect_equal(evp, c(13.2302, 10.8314, -0.3732))
+})
+
+
+test_that("return periods are equal to DWA", {
+  rp <- c(5, 10, 20, 50, 100)
+  quant <- round(evquantile(fit, return.period = rp)$T_Years_Event[, "gev"], 1)
+
+  expect_equal(unname(quant), c(17.2, 30.1, 45.4, 71.7, 98.1))
+})
